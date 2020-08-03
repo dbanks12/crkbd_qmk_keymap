a76f4fe168071ac1a53dd7017922c8d56e5defe5
dbanks12 <david.banks.engineer@gmail.com>
2020-08-02 21:05:03 -0400

    tweaked tapping term

--- config.h
+++ config.h
@@ -39,7 +39,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
+#define TAPPING_TERM 120
 //
 //#ifdef RGBLIGHT_ENABLE
 //    #undef RGBLED_NUM
